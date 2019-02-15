8519287511217f30eabbe85d26ee4dfd8e50e680
brentru <robots199@me.com>
2019-02-15 16:50:05 -0500

    add GET and POST methods, refactor send and receive methods

--- adafruit_io.py
+++ adafruit_io.py
@@ -16,7 +16,7 @@ class Client(object):
         :param str api_version: Adafruit IO REST API Version
         """
         self.api_version = api_version
-        self.url = 'https://io.adafruit.com/api/'+ self.api_version + '/'
+        self.url = 'https://io.adafruit.com/api'
         self.username = username
         self.key = key
         self.wifi = wifi_manager
@@ -26,11 +26,30 @@ class Client(object):
     # TODO: Add Error Handling
 
     def _compose_path(self, path):
-      return self.url+self.username+path
+      return "{0}/{1}/{2}/{3}".format(self.url, self.api_version, self.username, path)
     
     def _create_data(self, data, latitude, longitude, elevation, timestamp):
       return {'value':data, 'lat':latitude, 'lon':longitude,
                 'ele':elevation, 'created_at':timestamp}
+
+    def _post(self, path, packet):
+        """
+        Send data to Adafruit IO.
+        :param str path: Composed URL
+        :param json packet: JSON data to send to Adafruit IO
+        """
+        response = self.wifi.post(
+            path,
+            json = packet,
+            headers = self.headers)
+        response.close()
+
+    def _get(self, path):
+        response = self.wifi.get(
+            path,
+            headers=self.headers)
+        return response.json()
+        response.close()
 
     def send(self, feed, data, lat=None, lon=None, ele=None, created_at=None):
         """
@@ -42,22 +61,14 @@ class Client(object):
         :param int ele: Optional elevation
         :param string created_at: Optional date/time string
         """
-        path = self._compose_path("/feeds/{0}/data".format(feed))
+        path = self._compose_path("feeds/{0}/data".format(feed))
         packet = self._create_data(data, lat, lon, ele, created_at)
-        response = self.wifi.post(
-            path,
-            json = packet,
-            headers=self.headers)
-        #print(response.json())
-        response.close()
+        self._post(path, packet)
 
     def receive(self, feed):
         """Return the most recent value for the specified feed.
         :param string feed: Name/Key/ID of Adafruit IO feed.
         """
-        path = self.url+self.username+"/feeds/"+feed+"/data/last"
-        response = self.wifi.get(
-            path,
-            headers=self.headers)
-        return response.json()
-        response.close()
+        path = self._compose_path("feeds/{0}/data/last".format(feed))
+        return self._get(path)
+        
